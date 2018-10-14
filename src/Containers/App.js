bc7ea12b9d7a669f1a2851c929742223c6dad258
Pruthvi <pruthvinoruthvi@gmail.com>
2018-10-14 20:08:50 +0530

    adding Redux

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,40 +1,46 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll';
 import './App.css';
 
+import {setSearchField,requestRobots} from '../actions';
+
+const mapStateToProps=state=>{
+	return{
+		searchField:state.searchRobots.searchField,
+		robots:state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps=(dispatch)=>{
+	return{
+  onSearchChange: (event)=>dispatch(setSearchField(event.target.value)),
+  onRequestRobots: ()=> dispatch(requestRobots())
+}
+}
 
 class App extends React.Component{
-	constructor () {
-	super()
-	this.state = {
-	robots: [], 
-	searchfield:''
-  }
-}
+	
 componentDidMount(){
-	fetch('https://jsonplaceholder.typicode.com/users') //fetch is a method on the window object.
-	.then(response=> response.json())
-	.then(users=> {this.setState({robots:users})});
-}
+          this.props.onRequestRobots();
+      }
 
-onSearchChange=(event)=>{
-	    this.setState({searchfield:event.target.value})
-  
-}
 
 render() {
-	   const {robots, searchfield}= this.state;
+	   const {searchField,onSearchChange,robots, isPending}=this.props;
 	   const filteredRobots =robots.filter(robot=>{
-       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+       return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-	 return !robots.length?
+	 return isPending?
 	 	<h1> loading</h1>:
        (
 		<div className='tc'>
-		<h1 className='f1'> RoboFriends</h1>
-		<SearchBox searchChange={this.onSearchChange}/>
+		<h1 className='f'> RoboFriends</h1>
+		<SearchBox searchChange={onSearchChange}/>
 		<Scroll>
         <CardList robots={filteredRobots}/>
         </Scroll>
@@ -45,4 +51,4 @@ render() {
      }
 }
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react'; //webpack is underneath the hood that does imports
 import ReactDOM from 'react-dom'; //ReactNAtive for mobile phones
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';// react allows us to add css files to each component. "./" means same folder
 import App from './Containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import {searchRobots,requestRobots} from './reducers';
 import 'tachyons';
 //ReactDOM-Package uses function render. render function 
 
-ReactDOM.render(<App/>, document.getElementById('root')); 
+const logger=createLogger();
 
+const rootReducer=combineReducers({searchRobots,requestRobots})
+
+const store= createStore(rootReducer,applyMiddleware(thunkMiddleware ,logger));
+
+ReactDOM.render(
+	            <Provider store={store}>
+	            <App />
+	            </Provider>, document.getElementById('root')); 
 registerServiceWorker();
